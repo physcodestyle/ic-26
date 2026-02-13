e4fab94a7e17d0e566cefef42b91f902815156a9
Igor Korovchenko <igsekor@gmail.com>
2026-02-13 18:17:28 +0300

    Добавляет программу для отрисовки параболы с помощью псевдографики

---
+++ practice-1/plot.py
@@ -0,0 +1,19 @@
+def print_plot_1d(function, x_min, x_max, point_count):
+    dx = (x_max - x_min) / point_count
+    for i in range(point_count):
+        x = x_min + dx * i
+        y = function(x)
+        point = ' ' * int(y) + '·'
+        print(point)
+
+
+def f(x):
+    return x * x
+
+
+min = 0
+max = 10
+points = 10
+
+
+print_plot_1d(f, min, max, points)
