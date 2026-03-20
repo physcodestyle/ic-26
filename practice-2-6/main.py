957eda81ed0379b99bdbb85f886f7d8fb9f42fca
Igor Korovchenko <igsekor@gmail.com>
2026-03-20 18:41:32 +0300

    Добавляет результат работы на шестой практике

--- practice-2-6/main.py
+++ practice-2-6/main.py
@@ -1,13 +1,16 @@
 import sys
 from modules.import_data import import_csv_data
 from modules.print_data import print_table, print_plot, PlotType
+from modules.intrepolation import interpolation_1d, IntFuncType
 
 
 def run():
     data_file = sys.argv[1]
     data = import_csv_data(filename=data_file, delimiter=",")
-    print_plot(data=data, plot_type=PlotType.GRAPH_TYPE)
-
+    x_data = data["cols"][0]
+    y_data = data["cols"][3]
+    int_func = interpolation_1d(x_list=x_data, y_list=y_data, int_func_type="IntFuncType.Polynomial1")
+    print(int_func)
 
 if __name__ == '__main__':
     run()
---
+++ practice-2-6/modules/intrepolation.py
@@ -0,0 +1,30 @@
+import sys
+from enum import Enum
+
+
+CALCULATION_ERROR = 0.0000001
+
+
+class IntFuncType(Enum):
+    Polynomial1 = 1
+
+
+def interpolation_1d(x_list, y_list, int_func_type = IntFuncType.Polynomial1):
+    if int_func_type == IntFuncType.Polynomial1:
+        x_min = min(x_list)
+        x_max = max(x_list)
+        y_min = min(y_list)
+        y_max = max(y_list)
+        for index in range(len(x_list)):
+            if abs(x_list[index] - x_min) < CALCULATION_ERROR:
+                y_min = y_list[index]
+            elif abs(x_list[index] - x_max) < CALCULATION_ERROR:
+                y_max = y_list[index]
+        a = (y_max - y_min) / (x_max - x_min)
+        y_0 = y_min
+        x_b = -x_min
+        y_b = a * x_b
+        b = y_0 + y_b
+        return f"{a} * x + {b}"
+    else:
+        return ""
